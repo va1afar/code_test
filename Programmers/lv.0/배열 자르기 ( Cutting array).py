d8f660356e6b38606083fe553feadee7e548b474
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-16 00:10:43 +0900

    Create 배열 자르기 ( Cutting array).py

---
+++ Programmers/lv.0/배열 자르기 ( Cutting array).py
@@ -0,0 +1,8 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/120833
+
+def solution(numbers, num1, num2):
+    answer = []
+    
+    answer = numbers[num1:num2+1]
+    
+    return answer
