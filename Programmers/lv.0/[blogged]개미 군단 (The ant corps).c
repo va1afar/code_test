dc402c473c26edf052a5d48bedf46b1de7b5012c
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-16 00:56:11 +0900

    Create [blogged]개미 군단 (The ant corps).c

---
+++ Programmers/lv.0/[blogged]개미 군단 (The ant corps).c
@@ -0,0 +1,20 @@
+// link :: https://school.programmers.co.kr/learn/courses/30/lessons/120837
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int hp) {
+    int answer = 0;
+
+    answer += (hp/5);
+    hp %= 5;
+        
+    answer += (hp/3);
+    hp %= 3;
+    
+    answer += (hp/1);
+    hp %= 1;
+        
+    return answer;
+}
