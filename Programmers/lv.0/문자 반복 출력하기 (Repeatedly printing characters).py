afa91a234015ce45909c3105e3dc1afed8366071
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-16 00:14:51 +0900

    Create 문자 반복 출력하기 (Repeatedly printing characters).py

---
+++ Programmers/lv.0/문자 반복 출력하기 (Repeatedly printing characters).py
@@ -0,0 +1,9 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/120825
+
+def solution(my_string, n):
+    answer = ''
+
+    for i in range(len(my_string)):
+        answer += (my_string[i])*n
+    
+    return answer
