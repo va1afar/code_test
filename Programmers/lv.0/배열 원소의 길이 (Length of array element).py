4449d270d4a2f883becbae3949744f93bc2c5fa1
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-16 00:03:26 +0900

    Create 배열 원소의 길이 (Length of array element).py

---
+++ Programmers/lv.0/배열 원소의 길이 (Length of array element).py
@@ -0,0 +1,9 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/120854
+
+def solution(strlist):
+    answer = []
+    
+    for i in range(len(strlist)):
+        answer.append(len(strlist[i]))
+    
+    return answer
