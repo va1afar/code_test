56148c43627952d00f16a32ba4ddb2c4b37c7ab4
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-16 00:52:19 +0900

    Create 중앙값 구하기 (Find the median value).py

---
+++ Programmers/lv.0/중앙값 구하기 (Find the median value).py
@@ -0,0 +1,9 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/120811
+
+def solution(array):
+    answer = 0
+    
+    array.sort()
+    answer = array[len(array)//2]
+    
+    return answer
