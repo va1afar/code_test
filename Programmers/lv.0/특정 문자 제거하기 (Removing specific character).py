04760a9faa60c79b1deb05d472b07e3f2ee8dda8
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-16 00:41:00 +0900

    Create 특정 문자 제거하기 (Removing specific character).py

---
+++ Programmers/lv.0/특정 문자 제거하기 (Removing specific character).py
@@ -0,0 +1,8 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/120826
+
+def solution(my_string, letter):
+    answer = ''
+    
+    answer = my_string.replace(letter, '')
+    
+    return answer
