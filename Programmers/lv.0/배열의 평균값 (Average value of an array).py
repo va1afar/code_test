970dffab31c4b99b0302c7fd16e67f42e5cd8eaf
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-15 23:58:02 +0900

    Create 배열의 평균값 (Average value of an array).py

---
+++ Programmers/lv.0/배열의 평균값 (Average value of an array).py
@@ -0,0 +1,10 @@
+#link :: https://school.programmers.co.kr/learn/courses/30/lessons/120817
+
+def solution(numbers):
+    # variable.
+    answer = 0
+    
+    #calculation.
+    answer = sum(numbers) / len(numbers)
+    
+    return answer
