cf2899d2bd91899f6d9541555f09cc422c8d0213
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-16 00:07:14 +0900

    Create 배열 뒤집기 (Flip array).py

---
+++ Programmers/lv.0/배열 뒤집기 (Flip array).py
@@ -0,0 +1,10 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/120821
+
+def solution(num_list):
+    # variable.
+    answer = []
+    
+    # calculation.
+    answer = list(reversed(num_list)) 
+    
+    return answer
