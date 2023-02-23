191d59d011610a14b3b6adc7c6824c164f69cc61
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-23 20:42:59 +0900

    Create 짝수와 홀수 (even and odd numbers).py

---
+++ Programmers/lv.1/짝수와 홀수 (even and odd numbers).py
@@ -0,0 +1,11 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+def solution(num):
+    answer = ''
+    
+    if num % 2 == 0:
+        answer = 'Even'
+    else:
+        answer = 'Odd'
+    
+    return answer
