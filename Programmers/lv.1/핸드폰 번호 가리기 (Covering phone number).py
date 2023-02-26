c578d3a8d92cf73b7fbf7121acf55d4d5cf73e83
va1afar <62978894+va1afar@users.noreply.github.com>
2023-02-27 08:43:35 +0900

    Create 핸드폰 번호 가리기 (Covering phone number).py

---
+++ Programmers/lv.1/핸드폰 번호 가리기 (Covering phone number).py
@@ -0,0 +1,8 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/12948
+
+def solution(phone_number):
+    answer = ''
+    
+    answer = '*'*(len(phone_number)-4)+ phone_number[-4:] 
+
+    return answer
