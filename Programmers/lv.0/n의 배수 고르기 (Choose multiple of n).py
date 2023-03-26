ee26e735eed87edd600360f3067d7019e6482635
va1afar / jin <va1afar.k@gmail.com>
2023-03-26 23:36:31 +0900

    2023.03.26 solved

---
+++ Programmers/lv.0/n의 배수 고르기 (Choose multiple of n).py
@@ -0,0 +1,10 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/120905
+
+def solution(n, numlist):
+    answer = []
+    
+    for i in numlist:
+        if i % n == 0:
+            answer.append(i)
+    
+    return answer
