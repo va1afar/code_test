5f817f3579856f7d0dab9dbf6ad9ba289a074f88
va1afar / jin <va1afar.k@gmail.com>
2023-04-12 22:35:39 +0900

    2023.04.12 commit

--- Programmers/lv.1/체육복 (gym clothes).py
+++ Programmers/lv.1/체육복 (gym clothes).py
@@ -5,7 +5,6 @@ def solution(n, lost, reserve):
     reserve.sort()
     
     # 同じ番号の場合や±１の場合、「reserve」から削除し「cnt」＋1
-    # 最終的に体育福を持ってた人＋「cnt」で計算しました。
     # ただ、取られた人が貸してくれる場合もあるので、
     # 「i+1」 の場合その場合をチェックする必要があります。
     for i in lost:
