b93b2989071b213010198f0a256bc548afcb26bf
va1afar / jin <va1afar.k@gmail.com>
2023-04-28 23:02:08 +0900

    2023.04.28 commit
    [Programmers] coding test solved.

---
+++ Programmers/lv.2/점프와 순간 이동 (Jump and teleport).py
@@ -0,0 +1,13 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/12980
+
+def solution(n):
+    answer = 0
+    
+    while n > 0:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n -= 1
+            answer += 1
+
+    return answer
