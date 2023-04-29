2f07584ee994b76866339770b70c1a3cbbc1cc79
va1afar / jin <va1afar.k@gmail.com>
2023-04-29 23:37:08 +0900

    2023.04.29 modified
    Blog link added

--- Programmers/lv.2/[Blogged] 점프와 순간 이동 (Jump and teleport).py
+++ Programmers/lv.2/[Blogged] 점프와 순간 이동 (Jump and teleport).py
@@ -1,5 +1,7 @@
 # link :: https://school.programmers.co.kr/learn/courses/30/lessons/12980
 
+# blogged link :: https://medium.com/@va1afar.biz/programmers-coding-test-lv-2-jump-and-teleport-3d37e0f5fb4
+
 def solution(n):
     answer = 0
     
