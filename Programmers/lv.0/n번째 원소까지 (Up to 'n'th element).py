41b8747963cbea3a4ed8d81c730116f0650405f0
va1afar / jin <va1afar.k@gmail.com>
2023-05-09 23:53:17 +0900

    2023.05.09 commit
    [Programmers] coding test solved.

---
+++ Programmers/lv.0/n번째 원소까지 (Up to 'n'th element).py
@@ -0,0 +1,9 @@
+# link :: https://school.programmers.co.kr/learn/courses/30/lessons/181889
+
+def solution(num_list, n):
+    answer = []
+    
+    for i in range(0, n):
+        answer.append(num_list[i])
+    
+    return answer
